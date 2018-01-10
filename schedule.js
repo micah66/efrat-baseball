76c3234fe9e8c6da9a490d4dc10ccd513a90720b
Micah Gordon <micahgordon66@gmail.com>
2018-01-10 14:06:59 +0200

    remove games array and load schedule via JSON file

--- schedule.js
+++ schedule.js
@@ -11,32 +11,26 @@ const colorOfTeam = {
   'World Ventures': 'world'
 }
 
-var games = [
-    {date: "Sept 1st", time: "12:45", away: "Pina Chama", home: "Scorpions"},
-    {date: "Sept 1st", time: "14:45", away: "Papa Spice", home: "World Ventures"},
-    {date: "Sept 8th", time: "12:45", away: "Scorpions", home: "World Ventures"},
-    {date: "Sept 8th", time: "14:45", away: "Pina Chama", home: "Papa Spice"},
-    {date: "Sept 15th", time:  "12:45", away: "World Ventures", home: "Pina Chama"},
-    {date: "Sept 15th", time:  "14:45", away: "Papa Spice", home: "Scorpions"},
-    {date: "Sept 20th", time:  "12:45", away: "Pina Chama", home: "Papa Spice"},
-    {date: "Sept 20th", time:  "14:45", away: "World Ventures", home: "Scorpions"},
-    {date: "Oct 13th", time: "12:45", away: "Scorpions", home: "Papa Spice"},
-    {date: "Oct 13th", time: "14:45", away: "Pina Chama", home: "World Ventures"},
-    {date: "Oct 20th", time: "12:45", away: "World Ventures", home: "Papa Spice"},
-    {date: "Oct 20th", time: "14:45", away: "Scorpions", home: "Pina Chama"},
-    {date: "Oct 27th", time: "12:45", away: "Scorpions", home: "World Ventures"},
-    {date: "Oct 27th", time: "14:45", away: "Papa Spice", home: "Pina Chama"}
-]
 
-const htmlString = games
-  .map((game) => '' +
-    '<tr>' +
-      '<td>' + game.date + '</td>' +
-      '<td>' + game.time + '</td>' +
-      '<td class="' + colorOfTeam[game.away] + '">' + game.away + '</td>' +
-      '<td class="' + colorOfTeam[game.home] + '">' + game.home + '</td>' +
-    '</tr>'
-  )
-  .join('')
+const schedule = new XMLHttpRequest();
+schedule.onreadystatechange = function() {
+  if (schedule.readyState == 4 && schedule.status == 200) {
+    const jschedule = JSON.parse(schedule.responseText)
+    const htmlString = jschedule.games
+      .map((game) => '' +
+        '<tr>' +
+          '<td>' + game.date + '</td>' +
+          '<td>' + game.time + '</td>' +
+          '<td class="' + colorOfTeam[game.away] + '">' + game.away + '</td>' +
+          '<td class="' + colorOfTeam[game.home] + '">' + game.home + '</td>' +
+        '</tr>'
+      )
+      .join('')
+      populateElementWithText('js-schedule', htmlString)
 
-populateElementWithText('js-schedule', htmlString)
+
+    //console.log(jschedule.games)
+  }
+}
+schedule.open("GET", "schedule.JSON", true)
+schedule.send()
