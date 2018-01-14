8a1dc5fb7d01f10de51fa820e3cf188b87913df7
Micah Gordon <micahgordon66@gmail.com>
2018-01-14 20:16:27 +0200

    Close issue #5: simplify XHR

--- schedule.js
+++ schedule.js
@@ -1,27 +1,8 @@
-function populateElementWithText (id, text) {
-  var element = document.getElementById(id)
-  element.innerHTML = text
-}
-
 const colorOfTeam = {
   'Pina Chama': 'pina',
   'Scorpions': 'scorpions',
   'Papa Spice': 'papa',
   'World Ventures': 'world'
-}
-
-function fetchJSON(file, callback) {
-  const xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
-    const XHR_DONE = 4
-    const HTTP_OK = 200
-    if (xhr.readyState == XHR_DONE && xhr.status == HTTP_OK) {
-      const data = JSON.parse(xhr.responseText)
-      callback(data)
-    }
-  }
-  xhr.open("GET", file, true)
-  xhr.send()
 }
 
 fetchJSON('schedule.json', function (games) {
